9deb7290afbf641cf8ce724589482071bf325d51
shankhalp13 <shankhalppradhan2003@gmail.com>
2024-09-20 19:38:26 +0530

    Go Datatypes

---
+++ 2_simple_values/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int
+	fmt.Println(1 + 1)
+	//string
+	fmt.Println("Hello GoLang")
+	//boolean
+	fmt.Println(true)
+	//float
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+
+}
